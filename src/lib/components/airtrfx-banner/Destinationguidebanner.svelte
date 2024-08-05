818cd3710dc48a59c40f94caa6b89cd66205e737
josevch <javch1995@gmail.com>
2024-08-05 17:52:25 -0500

    init for destinationsguide banners

---
+++ src/lib/components/airtrfx-banner/Destinationguidebanner.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let href
+    export let alt = 'not set'
+    export let mobileImage
+    export let desktopImage
+</script>
+
+<a {href} target="_self" rel="noopener"> 
+<picture> 
+    <source media="(min-width: 768px)" srcset="https://cm-marketing.directus.app/assets/{desktopImage}" /> 
+    <img id="cross-promo-banner" src="https://cm-marketing.directus.app/assets/{mobileImage}" {alt} class="img-responsive crosspromo" style="display: block; margin: 0 auto;" /> 
+</picture> 
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+  <script>
+    import {Root as Dgbanner} from "$lib/components/airtrfx-banner";
+	</script>
+  
+  <Dgbanner href="#" alt="Best Fare Finder - Crosspromo" mobileImage="ab345cc8-9989-4f47-902d-23b611555151" desktopImage="ef1f9868-62fe-4900-837f-777f1b618bfd"/>
+
+  <Dgbanner href="#" alt="Best Fare Finder - Crosspromo" mobileImage="b0b448d9-c69e-427f-ab2c-584d570d769a" desktopImage="5191bfe6-4479-41b9-9bc5-606f065bcd20"/>
+
+  <Dgbanner href="#" alt="Best Fare Finder - Crosspromo" mobileImage="a53368cb-6fe2-4c4a-9591-93c8b5114c09" desktopImage="2c745f28-642b-4253-8f0b-97ddcd196394"/>
+
+
+
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="95727ece-2a72-4941-bdd4-6db7e93c5598" desktopImage="67820d73-c3b6-45dc-90fc-66ae2842a616"/>
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="b0b448d9-c69e-427f-ab2c-584d570d769a" desktopImage="66afdafc-d296-459f-ad36-31cf3d9b104e"/>
+
+  
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="67820d73-c3b6-45dc-90fc-66ae2842a616" desktopImage="fbd65ebc-686a-489a-b595-06bca121481c"/>
