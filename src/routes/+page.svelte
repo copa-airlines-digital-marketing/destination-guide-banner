4564f2e28ca87b6726273d715441a7869ada1d73
Jose Von Chong <javch1995@gmail.com>
2024-09-06 17:40:57 -0500

    flagged to export

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,30 @@
 
 
 
-
   <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="95727ece-2a72-4941-bdd4-6db7e93c5598" desktopImage="67820d73-c3b6-45dc-90fc-66ae2842a616"/>
   <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="b0b448d9-c69e-427f-ab2c-584d570d769a" desktopImage="66afdafc-d296-459f-ad36-31cf3d9b104e"/>
 
   
   <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="67820d73-c3b6-45dc-90fc-66ae2842a616" desktopImage="fbd65ebc-686a-489a-b595-06bca121481c"/>
+
+  <!-- Curazao -->
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Vuelos a Curazao" mobileImage="c098623e-6f49-4bdd-8a4f-a67451bd3ece?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="b93ef0a0-50fc-403b-9f39-6e34d25dba94?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Flights to Curazao" mobileImage="fc0e63b6-f8ef-4c27-8351-ee93b4c27419?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="7f597e09-2744-417b-a817-4730194e2f2b?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Flights to Curazao" mobileImage="f0e0cb63-881c-4b85-a15b-4d5eed4bdeca?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="fd76138e-5a7b-4e5c-b7be-da48873d1f78?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Flights to Curazao" mobileImage="6e5dfef2-2fa0-4c92-b15a-1d4681ee734e?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="5d47910f-a2ea-4fbb-9edc-e96d132f6c58?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
+
+
+
+
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Vuelos a Aruba" mobileImage="dca3c82f-d0aa-4902-9e14-ee0a34428392?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="42031075-4e90-426c-aa1d-47a3b2512136?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Flights to Aruba" mobileImage="b647d754-3502-4fe2-b8b8-3c755072b59e?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="067bf18b-93a7-4b92-acb5-e33bc2494171?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Voos para Aruba" mobileImage="0be30bfa-8e72-42b5-8feb-444ce6456fda?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="de7bb886-1f8a-41fd-98ee-5f83dbba2280?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
+
+  <Dgbanner href="https://www.stopoverinpanama.com" alt="Flights to Aruba" mobileImage="ce869602-3263-4299-b368-eb383697b8d6?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="b1468a82-e90d-42c9-b27c-adfd29521dae?access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ"/>
