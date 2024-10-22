5b146282ab0e951b870e141c12b97b7b9c13b805
josevch <javch1995@gmail.com>
2024-10-22 09:32:55 -0500

    Stopover 2.0 DGB ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,11 @@
 
   <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="95727ece-2a72-4941-bdd4-6db7e93c5598" desktopImage="67820d73-c3b6-45dc-90fc-66ae2842a616"/>
   <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="b0b448d9-c69e-427f-ab2c-584d570d769a" desktopImage="66afdafc-d296-459f-ad36-31cf3d9b104e"/>
-
-  
   <Dgbanner href="https://www.stopoverinpanama.com" alt="Best Fare Finder - Crosspromo" mobileImage="67820d73-c3b6-45dc-90fc-66ae2842a616" desktopImage="fbd65ebc-686a-489a-b595-06bca121481c"/>
+
+
+
+  <Dgbanner href="https://www.panama-stopover.com" alt="Stopover in Panama" mobileImage="6da18cf3-a464-4575-a8d1-ef6e115e5c4f?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="5bc75aef-0265-467b-b786-426d3ec2cf35?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ&width=1200"/>
+  <Dgbanner href="https://www.panama-stopover.com" alt="Stopover en Panama" mobileImage="83367d2f-569a-462b-b569-89bea89485b3?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="9baabbd5-1dba-40e4-8451-1a120b6543e4?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ&width=1200"/>
+  <Dgbanner href="https://www.panama-stopover.com" alt="Stopover na Panama" mobileImage="1f8630b5-e481-4993-b53b-c05c24ef477d?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="88cb2f2c-cefe-4b8c-a9f7-bcf3ea0aeca4?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ&width=1200"/>
+  <Dgbanner href="https://www.panama-stopover.com" alt="Stopover au Panama" mobileImage="ef0761c4-05f4-4ab3-bf2e-02c6af22ce0d?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ" desktopImage="c98a53a0-a04c-4f7e-aa1c-cd734cfe9371?format=auto&access_token=2NYgIiKkCP6FNY4Jj31_h4d5VKkSSVbZ&width=1200"/>
